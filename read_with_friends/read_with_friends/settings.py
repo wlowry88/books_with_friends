299131a5c470173f07a2ce49a80aad0c2883c48e
Will Lowry <william.c.lowry@gmail.com>
2015-04-26 15:53:16 -0700

    add __init__.py for module recognition, adjusted settings, moved urls module, adjusted requirements

--- read_with_friends/read_with_friends/settings.py
+++ read_with_friends/read_with_friends/settings.py
@@ -121,7 +121,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'read_with_friends.urls'
+ROOT_URLCONF = 'urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'read_with_friends.wsgi.application'
@@ -139,8 +139,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'read_with_friends',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
